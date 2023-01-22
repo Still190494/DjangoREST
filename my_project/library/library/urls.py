550bab1e79be7237a09a08d45bd466bbc0ef4f19
Still190494 <Still190494@ya.ru>
2023-01-22 13:35:12 +0300

    ToDo_model

--- my_project/library/library/urls.py
+++ my_project/library/library/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
-from TODO.views import ProjectModelViewSet
+from TODO.views import ProjectModelViewSet, ToDoModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
 router.register('project', ProjectModelViewSet)
+router.register('todo', ToDoModelViewSet)
 
 
 urlpatterns = [
