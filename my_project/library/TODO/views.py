c107eb49cfaafec28643474803a70a6657195ee5
Still190494 <Still190494@ya.ru>
2023-01-28 13:53:39 +0300

    filters

--- my_project/library/TODO/views.py
+++ my_project/library/TODO/views.py
@@ -1,10 +1,8 @@
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
 from rest_framework.pagination import LimitOffsetPagination
-
-
-
 
 
 class ProjectPagination(LimitOffsetPagination):
@@ -19,9 +17,12 @@ class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
     pagination_class = ProjectPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name_project']
 
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
     pagination_class = ToDoPagination
+    filterset_fields = ['project_note']
--- my_project/library/library/settings.py
+++ my_project/library/library/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'TODO',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -139,6 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.AdminRenderer',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 
