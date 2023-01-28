edc0a42c14261258a70098a908d26b9ac4c016db
Still190494 <Still190494@ya.ru>
2023-01-28 11:34:36 +0300

    paginatin

--- my_project/library/TODO/views.py
+++ my_project/library/TODO/views.py
@@ -1,13 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+
+
+
+
+
+class ProjectPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectPagination
 
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    pagination_class = ToDoPagination
