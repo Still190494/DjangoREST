27df7be168e7ef566dc14b0d2237b16ef17f3fd8
Still190494 <100068017+Still190494@users.noreply.github.com>
2023-02-13 19:31:18 +0300

    Lesson 7 (#7)
    * login
    
    * fix
    
    * fix2
    
    * lesson_7
    
    * fix_lesson_7

--- my_project/library/frontend/src/App.js
+++ my_project/library/frontend/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import UserList from './components/User.js';
 import ProjectList from './components/Project';
 import TodoList from './components/Todo';
-import axios from 'axios'
-import {HashRouter, Route, Link, Switch} from 'react-router-dom'
+import LoginForm from './components/Auth.js';
+import axios from 'axios';
+import {HashRouter, Route, Link, Switch, BrowserRouter} from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 class App extends React.Component {
 constructor(props) {
@@ -17,58 +19,112 @@ this.state = {
 }
 }
 
+set_token(token) {
+const cookies = new Cookies()
+cookies.set('token', token)
+this.setState({'token': token}, ()=>this.load_data())
+}
+
+is_authenticated() {
+return this.state.token != ''
+}
+
+logout() {
+this.set_token('')
+}
+
+get_token_from_storage() {
+const cookies = new Cookies()
+const token = cookies.get('token')
+this.setState({'token': token}, ()=>this.load_data())
+}
+
+get_token(login, password) {
+axios.post('http://127.0.0.1:8000/api-token-auth/', {username: login, password: password})
+.then(response => {
+this.set_token(response.data['token'])
+}).catch(error => alert('Неверный логин или пароль'))
+}
+
+get_headers() {
+let headers = {
+'Content-Type': 'application/json'
+}
+if (this.is_authenticated())
+{
+headers['Authorization'] = 'Token ' + this.state.token
+}
+return headers
+}
+
+load_data() {
+    const headers = this.get_headers()
+    axios.get('http://127.0.0.1:8000/api/users/', {headers})
+    .then(response => {
+    const users = response.data.results
+    this.setState(
+    {
+    'users': users
+    }
+    )
+    }).catch(error => console.log(error))
+    this.setState({users: []})
+
+    axios.get('http://127.0.0.1:8000/api/project/', {headers})
+    .then(response => {
+    const projects = response.data.results
+    this.setState(
+    {
+    'projects': projects
+    }
+    )
+    }).catch(error => console.log(error))
+    this.setState({projects: []})
+
+    axios.get('http://127.0.0.1:8000/api/todo/', {headers})
+    .then(response => {
+    const todos = response.data.results
+    this.setState(
+    {
+    'todos': todos
+    }
+    )
+    }).catch(error => console.log(error))
+    this.setState({todos: []})
+
+}
+
 
 componentDidMount() {
-axios.get('http://127.0.0.1:8000/api/users/')
-.then(response => {
-const users = response.data.results
-this.setState(
-{
-'users': users
-}
-)
-}).catch(error => console.log(error))
-
-axios.get('http://127.0.0.1:8000/api/project/')
-.then(response => {
-const projects = response.data.results
-this.setState(
-{
-'projects': projects
-}
-)
-}).catch(error => console.log(error))
-
-axios.get('http://127.0.0.1:8000/api/todo/')
-.then(response => {
-const todos = response.data.results
-this.setState(
-{
-'todos': todos
-}
-)
-}).catch(error => console.log(error))
+this.get_token_from_storage()
 }
 
 
 render() {
     return (
     <div className="App">
-    <HashRouter>
+    <BrowserRouter>
     <nav>
     <ul>
     <li><Link to='/'>Users</Link></li>
     <li><Link to='/project'>Projects</Link></li>
     <li><Link to='/todo'>Todos</Link></li>
+    <li>{this.is_authenticated() ? <button
+    onClick={()=>this.logout()}>Logout
+    </button> : <Link to='/login'>Login</Link>}</li>
     </ul>
     </nav>
+    <Switch>
     <Route exact path='/' component={() => <UserList
     users={this.state.users} />} />
     <Route exact path='/project' component={() => <ProjectList
     projects={this.state.projects} />} />
     <Route exact path='/todo' component={() => <TodoList
     todos={this.state.todos} />} />
-    </HashRouter>
+    <Route exact path='/login' component={() => <LoginForm
+    get_token={(login, password) => this.get_token(login, password)} />} />
+    </Switch>
+    </BrowserRouter>
     </div>
     )
     }
